22adab7957e987d2cd87a05b3df7e63fe1febe45
varun-cube <varun.chaudhari@cubehighways.com>
2024-08-23 17:11:36 +0530

    add binary search code with recursion to find specic element in sorted array

---
+++ binary-search/binary-search-recursion.js
@@ -0,0 +1,15 @@
+function binarySearch(arr, target, left, right) {
+    if (right < left) return false;
+
+    const mid = Math.floor((left + right) / 2);
+
+    if (arr[mid] === target) return true;
+
+    if (arr[mid] < target) {
+        return binarySearch(arr, target, mid + 1, right);
+    }
+    return binarySearch(arr, target, left, mid - 1);
+}
+
+const arr = [1, 3, 5, 7, 9, 11, 23, 30, 40];
+console.log(binarySearch(arr, 23, 0, arr.length - 1));
