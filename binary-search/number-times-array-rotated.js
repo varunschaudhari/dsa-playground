30245afc6e6d2774217a7c12970399e547b6b6b7
varun-cube <varun.chaudhari@cubehighways.com>
2024-08-24 19:55:35 +0530

    add code to check number of times sorted array is rotated

---
+++ binary-search/number-times-array-rotated.js
@@ -0,0 +1,34 @@
+function binarySearch(arr) {
+    let start = 0;
+    let end = arr.length - 1;
+
+    while (start <= end) {
+
+        // If the array is already sorted (no rotation)
+        if (arr[start] <= arr[end]) {
+            return start;
+        }
+
+        const mid = Math.floor((start + end) / 2);
+        // to restrict go out of bound, we added modulo n
+        const next = (mid + 1) % arr.length;
+        const prev = (mid - 1 + arr.length) % arr.length;
+        // Check if the mid element is the minimum element
+        if (arr[mid] <= arr[prev] && arr[mid] <= arr[next]) {
+            return mid;
+        }
+
+        // Decide which half to search next
+        if (arr[start] < arr[mid]) {
+            // Left part is sorted, go to the right part
+            start = mid + 1;
+        } else {
+            // Right part is sorted, go to the left part
+            end = mid - 1;
+        }
+    }
+    return -1; // In case the input is not a rotated sorted array
+}
+
+const arr = [11, 12, 15, 18, 2, 5, 6, 8];
+console.log(binarySearch(arr));
