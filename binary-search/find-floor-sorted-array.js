a2ee26f28d6ec943d23e540704d93d8fa0b67315
varun-cube <varun.chaudhari@cubehighways.com>
2024-08-25 17:42:57 +0530

    add code to check floor of the given number

---
+++ binary-search/find-floor-sorted-array.js
@@ -0,0 +1,22 @@
+function binarySearch(nums, target) {
+    let start = 0;
+    let end = nums.length - 1;
+    let res = nums[0];
+
+    while (start <= end) {
+        const mid = Math.floor((start + end) / 2);
+        console.log(target, nums[mid]);
+        if (nums[mid] < target) {
+            res = mid;
+            start = mid + 1;
+        } else {
+            end = mid - 1;
+        }
+    }
+
+    return res;
+}
+
+const arr = [1, 3, 5, 6];
+console.log(arr);
+console.log(binarySearch(arr, 5));
---
+++ binary-search/single-element-sorted-array.js
@@ -0,0 +1,6 @@
+function binarySearch(arr) {
+    
+}
+
+const nums = [1, 1, 2, 3, 3, 4, 4, 8, 8];
+console.log(binarySearch(arr));
