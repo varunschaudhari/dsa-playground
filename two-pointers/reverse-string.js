b529902c8110792e4030b5d4e59574b4d1b9abc6
varun-cube <varun.chaudhari@cubehighways.com>
2024-09-08 22:39:44 +0530

    add code to reverse string using two pointers

---
+++ two-pointers/reverse-string.js
@@ -0,0 +1,25 @@
+function reverseString(s) {
+    if (s.length === " ") return " ";
+    let left = 0;
+    let right = s.length - 1;
+
+    // Swap characters from the start and end
+    while (left < right) {
+        // Swap s[left] and s[right]
+        let temp = s[left];
+        s[left] = s[right];
+        s[right] = temp;
+        
+        // Move the pointers
+        left++;
+        right--;
+    }
+    return s;
+}
+
+// const s = ["h", "e", "l", "l", "o"];
+const s = ["H", "a", "n", "n", "a", "h"];
+console.log(reverseString(s));
+
+// time complexity --> O(n)
+// space complexity --> O(1)
