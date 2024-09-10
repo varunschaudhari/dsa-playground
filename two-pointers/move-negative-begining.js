8bf200fe49b55380264a3335d05ff817038a518b
varun-cube <varun.chaudhari@cubehighways.com>
2024-09-10 22:48:12 +0530

    add notes for move negative to begining of array

--- two-pointers/move-negative-begining.js
+++ two-pointers/move-negative-begining.js
@@ -15,3 +15,11 @@ function moveNegative(arr) {
 
 const arr = [1, -2, 3, -4, 5];
 console.log(moveNegative(arr));
+
+
+/*
+    Time Complexity: O(n)
+        The right pointer goes through the array once, and each negative element is swapped at most once, making the time complexity linear.
+    Space Complexity: O(1)
+        The function modifies the array in place, requiring no additional memory except for the left and right pointers.
+*/
